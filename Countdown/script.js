2409a608faf659f6593de3c707f58ad6e51746ef
PedroF3rr3ira <82749229+PedroF3rr3ira@users.noreply.github.com>
2022-03-02 17:19:39 -0300

    dando funcionalidade ao cronômetro

--- Countdown/script.js
+++ Countdown/script.js
@@ -4,7 +4,7 @@ const days = document.getElementById('counter-days');
 const hours = document.getElementById('counter-hours'); 
 const minutes = document.getElementById('counter-minutes'); 
 const seconds = document.getElementById('counter-seconds'); 
-
+let amountSeconds;
 
 function toggleModal(){
     const divModal = document.getElementById('modal-overlay');
@@ -18,10 +18,38 @@ function toggleModal(){
 }
 
 function tick(){
-    let amountSeconds = days.innerText*24;
+   if(amountSeconds>0){
+       amountSeconds--;
+       setCounter();
+   }else{
+   days.innerHTML="00";
+   hours.innerHTML="00";
+   minutes.innerHTML="00";
+   seconds.innerHTML='00';}
+}
+
+function setCounter(){
+    let daysF = Math.floor(amountSeconds/(24*60*60))//
+    let hoursF = Math.floor((amountSeconds%(24*60*60))/(60*60));
+    let minutesF = Math.floor(((amountSeconds%(24*60*60))%(60*60))/60);
+    let secondsF = Math.floor(((amountSeconds%(24*60*60))%(60*60))%60);
+    
+    const timeString = [daysF,hoursF,minutesF,secondsF].map(
+        unit=> String(unit).padStart(2,'0')
+    )
+    days.innerHTML = timeString[0];
+    hours.innerHTML = timeString[1];
+    minutes.innerHTML = timeString[2];
+    seconds.innerHTML = timeString[3];
+}
+
+function initAmountSeconds(){
+    amountSeconds = days.innerText*24;
     amountSeconds= (amountSeconds + Number(hours.innerText))*60;
     amountSeconds = (amountSeconds + Number(minutes.innerText))*60;
-    amountSeconds+=Number(seconds.innerText) ;
-    alert(amountSeconds)
+    amountSeconds+=Number(seconds.innerText);
+    
 }
-window.onload(tick())
+initAmountSeconds()
+setCounter();
+setInterval(tick,1000)
